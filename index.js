e999f0949c3dd9964d4ee5bcb59948fe694e04cd
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-04-09 16:40:42 -0400

    passed testing

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+if (someValue < 400 || someValue == 400) {
+  return 'This one is on me!';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (someValue > 400 && someValue < 2000) {
+  return 'That will be twenty bucks.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+else if (someValue > 2000 && someValue < 2500) {
+  return 'I will gladly take your thirty bucks.';
+}
+
+else if (someValue > 2500 ) {
+  return 'No can do.';
+
+}
+
+
+}
+
+
+function ternaryCheckCity(cityName){
+  if (cityName == 'NYC') {
+    return "Ok, sounds good.";
+  }
+
+  else if (cityName != 'NYC') {
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(tipType){
+  if (tipType == 'generous') {
+    return 'Thank you so much.';
+  }
+  else if (tipType == 'not as generous') {
+    return 'Thank you.';
+  }
+  else if (tipType == 'thanks for everything') {
+    return 'Bye.';
+  }
+  
 }
